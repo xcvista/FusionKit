41dbd1225f30c7d680fd5f3a805b9bcbc7d44050
xcvista <xcvista@me.com>
2013-03-04 22:38:18 +0800

    3A2: Finished code recycling. Start working on new kernel library.
    Actually, this kernel library is quite like the 1.x kernel, which use
    local variables to store objects. Extensionability is provided through
    use of NSMutableDictionary property tag in the base class.

--- Base64Kit/NSBase64DataEncoder.m
+++ Base64Kit/NSBase64DataEncoder.m
@@ -7,7 +7,18 @@
 //
 
 #import "NSBase64DataEncoder.h"
+#import "base64.h"
 
 @implementation NSBase64DataEncoder
+
++ (NSString *)base64StringOfData:(NSData *)data
+{
+    return [data base64EncodedString];
+}
+
++ (NSData *)dataOfBase64String:(NSString *)string
+{
+    return [NSData dataFromBase64String:string];
+}
 
 @end
