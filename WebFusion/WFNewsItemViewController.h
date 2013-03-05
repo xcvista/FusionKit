8c1184b952c72676c3c8f6758ce4cd032f8766a7
xcvista <xcvista@me.com>
2013-03-05 10:10:02 +0800

    3A18: News is fixed, no performance issue, branched for OS X

---
+++ WebFusion/WFNewsItemViewController.h
@@ -0,0 +1,13 @@
+//
+//  WFNewsItemViewController.h
+//  FusionKit.C
+//
+//  Created by Maxthon Chan on 13-3-5.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface WFNewsItemViewController : NSCollectionViewItem
+
+@end
